927141d7a4917a9eb745c47cb8d022e510a260a3
Tim Gold <timothygold@comcast.net>
2023-08-03 16:37:17 -0400

    added a comment.py file in models folder with class of comments

--- seeds.py
+++ seeds.py
@@ -1,4 +1,4 @@
-from app.models import User, Post 
+from app.models import User, Post, Comment 
 from app.db import Session, Base, engine 
 
 Base.metadata.drop_all(engine)
@@ -14,6 +14,8 @@ db.add_all([
   User(username='djiri4', email='gmidgley4@weather.com', password='password123')
 ])
 
+db.commit()
+
 db.add_all([
   Post(title='Donec posuere metus vitae ipsum', post_url='https://buzzfeed.com/in/imperdiet/et/commodo/vulputate.png', user_id=1),
   Post(title='Morbi non quam nec dui luctus rutrum', post_url='https://nasa.gov/donec.json', user_id=1),
@@ -24,6 +26,16 @@ db.add_all([
 
 db.commit()
 
+db.add_all([
+  Comment(comment_text='Nunc rhoncus dui vel sem.', user_id=1, post_id=2),
+  Comment(comment_text='Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo. Maecenas pulvinar lobortis est.', user_id=1, post_id=3),
+  Comment(comment_text='Aliquam erat volutpat. In congue.', user_id=2, post_id=1),
+  Comment(comment_text='Quisque arcu libero, rutrum ac, lobortis vel, dapibus at, diam.', user_id=2, post_id=3),
+  Comment(comment_text='In hac habitasse platea dictumst.', user_id=3, post_id=3)
+])
+
 db.commit()
+
+
 
 db.close()
