8f9b6ed97b493d46f78e894330ba068b6dbfc5e1
Tim Gold <timothygold@comcast.net>
2023-08-11 13:46:48 -0400

    made changes in the init__.py in the app and in the app/routes to include api routes

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask 
-from app.routes import home,  dashboard
+from app.routes import home,  dashboard,  api 
 from app.db import init_db
 from app.utils import filters
 
@@ -11,6 +11,8 @@ def create_app(test_config=None):
   app.config.from_mapping(
     SECRET_KEY='super_secret_key'
   )
+  app.register_blueprint(api)
+  
   @app.route('/hello')
   def hello():
     return 'hello world'
